0bb19dce200a891b9851d97c06d78fd2e3ab5319
Matt Shwery <mshwery@gmail.com>
2014-02-12 21:30:40 -0600

    sockjs instead of socket.io cuz it drops mic all the time

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,5 @@
+/*jshint laxcomma:true*/
+
 (function() {
 
   function urlsToHtml(text) {
@@ -15,16 +17,40 @@
 
 $(document).ready(function() {
 
-  var messages = [],
-      users = {},
-      typers = {},
-      socket = io.connect('/'),
-      user = 'Fedmate ' + parseInt(Math.random() * 1000, 10);
+  var messages      = []
+      , users       = {}
+      , typers      = {}
+      , sockjs_url  = '/chat'
+      , sockjs      = new SockJS(sockjs_url)
+      , user        = 'guest ' + new Date().getSeconds();
 
-  // dom els
-  var $newMessage = $('.new-message'),
-      $messages = $("#messages"),
-      $userList = $("#user-list");
+  // elements
+  var $newMessage = $('.new-message')
+      , $messages = $("#messages")
+      , $userList = $("#user-list")
+      , div       = $("#pings");
+
+  var print = function(message){
+    div.append($("<code>").text(message));
+    div.append($("<br>"));
+    div.scrollTop(div.scrollTop()+10000);
+  };
+
+  sockjs.onopen = function() {
+    print('Connected');
+  };
+  sockjs.onmessage = function(e) {
+    var data = JSON.parse(e.data);
+    if (data.message) {
+      addMessage(data.message);
+    } else {
+      print(data);
+    }
+    console.log(data);
+  };
+  sockjs.onclose = function() {
+    print('Closing connection.');
+  };
 
   function addUser(data) {
     if (data) {
@@ -55,9 +81,9 @@ $(document).ready(function() {
       messages.push(data.message);
       var klasses = ['user'];
 
-      klasses.push( (!data.username ? 'log' : ( (user == data.username) ? 'you' : '' )) );
+      klasses.push( (!data.user ? 'log' : ( (user == data.user) ? 'you' : '' )) );
 
-      var username = $("<span/>").addClass(klasses.join(' ')).text( '[' + moment(data.timestamp).format('h:mm:ss a') + '] ' + (data.username || 'Server') + ': ' ),
+      var username = $("<span/>").addClass(klasses.join(' ')).text( '[' + moment(data.timestamp).format('h:mm:ss a') + '] ' + (data.user || 'Server') + ': ' ),
           text = $("<span/>").text(data.message).linkify(),
           html = $("<li/>").append(username).append(text);
       $messages.append(html);
@@ -69,18 +95,22 @@ $(document).ready(function() {
   function sendMessage() {
     var text = $newMessage.val();
     if (text) {
-      socket.emit('send', { message: text, username: user });
+      sockjs.send(JSON.stringify({
+        message: {
+          user: user,
+          message: text
+        }
+      }));
       $newMessage[0].value = '';
       isTyping(false);
     }
   }
 
   // set up channel subscriptions
-  socket.on("message", addMessage);
-  socket.on("typing", addTyper);
-  socket.on("stopped typing", removeTyper);
-  socket.on("connected", addUser);
-  socket.on("disconnected", removeUser);
+  // socket.on("typing", addTyper);
+  // socket.on("stopped typing", removeTyper);
+  // socket.on("connected", addUser);
+  // socket.on("disconnected", removeUser);
 
   // handle if the user is typing or not
   var typingTimeout = null,
@@ -92,9 +122,9 @@ $(document).ready(function() {
     if (newValue) {
       if (typingTimeout) clearTimeout(typingTimeout);
       typingTimeout = setTimeout(isTyping, 5000);
-      socket.emit('typing', { username: user });
+      //socket.emit('typing', { username: user });
     } else {
-      socket.emit('stopped typing', { username: user });
+      //socket.emit('stopped typing', { username: user });
     }
   }
 
@@ -103,11 +133,25 @@ $(document).ready(function() {
     .on('keypress', function(e) {
       if ( e.keyCode == 13 ) {
         sendMessage();
+        e.preventDefault();
       }
     })
     .on('keyup', function(e) {
       var newValue = !!this.value.length;
       if (typing != newValue) isTyping(newValue);
-    });
+    })
+    .focus();
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+  });
+
+  // socket.onmessage = function (event) {
+  //   console.log(event);
+  //   var data = JSON.parse(event.data);
+  //   if (data.message) {
+  //     addMessage(data.message);
+  //   }
+  // };
 
 });
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 /*jshint laxcomma:true*/
 
-var express = require('express')
-  , io = require('socket.io')
-  , app = express()
-  , port = process.env.PORT || 5000;
+var http    = require('http')
+  , express = require('express')
+  , sockjs  = require('sockjs')
+  , redis   = require('redis')
+  , app     = express()
+  , port    = process.env.PORT || 5000;
 
 app.configure(function() {
   app.set('view engine', "jade");
@@ -12,28 +14,55 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
+// Redis pubsub
+var publisher = redis.createClient();
+
+// Sockjs server
+var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
+var sockjs_chat = sockjs.createServer(sockjs_opts);
+sockjs_chat.on('connection', function(conn) {
+  var browser = redis.createClient();
+  browser.subscribe('chat_channel');
+
+  // When we see a message on chat_channel, send it to the browser
+  browser.on("message", function(channel, message){
+      conn.write(message);
+  });
+
+  // When we receive a message from browser, send it to be published
+  conn.on('data', function(message) {
+      publisher.publish('chat_channel', message);
+  });
+});
+
+// Express server
+var server = http.createServer(app);
+
+sockjs_chat.installHandlers(server, {prefix:'/chat'});
+
+console.log(' [*] Listening on port ' + port );
+server.listen(port);
+
 app.get('/', function(req, res) {
   res.render('chat');
 });
 
-console.log('Listening on port ' + port);
+// var io = require('socket.io').listen(app.listen(port));
+// io.sockets.on('connection', function (socket) {
 
-var io = require('socket.io').listen(app.listen(port));
-io.sockets.on('connection', function (socket) {
+//   socket.emit('message', { message: 'welcome to the chat', timestamp: new Date().getTime() });
 
-  socket.emit('message', { message: 'welcome to the chat', timestamp: new Date().getTime() });
+//   socket.on('send', function (data) {
+//     data.timestamp = new Date().getTime();
+//     io.sockets.emit('message', data);
+//   });
 
-  socket.on('send', function (data) {
-    data.timestamp = new Date().getTime();
-    io.sockets.emit('message', data);
-  });
+//   socket.on('typing', function(data) {
+//     io.sockets.emit('typing', data);
+//   });
 
-  socket.on('typing', function(data) {
-    io.sockets.emit('typing', data);
-  });
+//   socket.on('stopped typing', function(data) {
+//     io.sockets.emit('stopped typing', data);
+//   });
 
-  socket.on('stopped typing', function(data) {
-    io.sockets.emit('stopped typing', data);
-  });
-
-});
+// });
